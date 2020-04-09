a3c1f6a966f79d8c1fea9427044312c7f04206bb
unknown <cudzich_m@outlook.com>
2020-04-09 11:15:52 -0500

    Added a list of heroes and enabled conditional detail view of a selected hero
    - Used *ngFor to display a list
    - Used *ngIf to conditionally execute/show a component
    - User can select a hero from list and view its details below
    - Added conditional styling with a [class.css-class]="some-condition" binding

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../hero';
+import { HEROES } from '../../mock-heroes';
 
 // @Component is a decorator function that specifies the Angular
 // metadata for the component.
@@ -11,14 +12,16 @@ import { Hero } from '../../hero';
 
 // You must export the class so it can be used elsewhere.
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id:1,
-    name: 'Winstorm'
-  };
-  
+heroes = HEROES;
+selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit(): void { // lifecycle hook, place initializing logic here
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id:1, name:'Dr. Nice' },
+  { id:2, name:'Narco'},
+  { id:3, name:'Bobasto'},
+  { id:4, name:'Kielbasa'},
+  { id:5, name:'Nina'},
+  { id:6, name:'Tiestooo'},
+  { id:7, name:'Dynama'},
+  { id:8, name:'Dermaa'},
+  { id:9, name:'Tornado'},
+  { id:10, name:'Trouble'}
+];
